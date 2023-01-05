bc1680f2e230f302c81148710274d8119c2496ad
Amir Riaz <116000783+Amiriaz@users.noreply.github.com>
2023-01-05 07:40:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+var btn = document.getElementById("btn");
+var light = document.getElementById("light");
+
+function toggleBtn(){
+  btn.classList.toggle("active")
+  light.classList.toggle("on");
+}
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
